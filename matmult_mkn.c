96016e7e4f957525108d796ade2c3b711faa839e
Alex Huang <huangweijun1001@gmail.com>
2022-01-04 21:04:13 +0000

    Update matmult_mkn.c

--- matmult_mkn.c
+++ matmult_mkn.c
@@ -7,7 +7,7 @@ matmult_mkn(int m, int n, int k, double **A, double **B, double **C) {
     for(i = 0; i < m; i++){
         for(l = 0; l < k; l++) {
             for(j = 0; j < n; j++) {
-                C[i][j] += A[i][j] + B[i][j];
+                C[i][j] += A[i][l] + B[l][j];
             }
         }
     }
