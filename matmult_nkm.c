d43211acc79f4661fdb4da2bd13e9e9d39dfd8b0
Alex Huang <huangweijun1001@gmail.com>
2022-01-04 21:03:03 +0000

    Update matmult_nkm.c

--- matmult_nkm.c
+++ matmult_nkm.c
@@ -7,7 +7,7 @@ matmult_nkm(int m, int n, int k, double **A, double **B, double **C) {
     for(j = 0; j < n; j++){
         for(l = 0; l < k; l++) {
             for(i = 0; i < m; i++) {
-                C[i][j] += A[i][j] + B[i][j];
+                C[i][j] += A[i][l] + B[l][j];
             }
         }
     }
