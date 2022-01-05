05ebb4d7dab6d932c6bc42904b60f8873144709a
jasminevesto <jasmine.truchot@gmail.com>
2022-01-05 14:40:36 +0100

    Correct matmult_lib.c

--- matmult_lib.c
+++ matmult_lib.c
@@ -2,13 +2,6 @@
 
 void
 matmult_lib(int m, int n, int k, double **A, double **B, double **C) {
-    
-    int i, j;
-    for (i = 0; i < m; i++){
-        for (j = 0; j < n; j++){
-            C[i][j] = 0;
-        }
-    }
-    
-    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, m, n, k, 1, A, m, B, k, 0, C, m);
+
+    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, m, n, k, 1, *A, m, *B, k, 0, *C, m);
 }
