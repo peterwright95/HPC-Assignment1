c81a9c9540f60ecbc5253858dc6986e51a6cdcc1
peterwright95 <49273091+peterwright95@users.noreply.github.com>
2022-01-05 10:27:56 +0100

    Update matmult_kmn.c

--- matmult_kmn.c
+++ matmult_kmn.c
@@ -6,7 +6,7 @@ matmult_kmn(int m, int n, int k, double **A, double **B, double **C) {
     for(l = 0; l < k; l++){
         for(i = 0; i < m; i++) {
             for(j = 0; j < n; j++) {
-                C[i][j] += A[i][l] + B[l][j];
+                C[i][j] += A[i][l] * B[l][j];
             }
         }
     }
