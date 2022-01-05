389a13118bf05e0e179860fc6d1dfbe612161159
peterwright95 <49273091+peterwright95@users.noreply.github.com>
2022-01-05 10:28:51 +0100

    Update matmult_nat.c

--- matmult_nat.c
+++ matmult_nat.c
@@ -6,7 +6,7 @@ matmul_nat(int m, int n, int k, double **A, double **B, double **C) {
     for(i = 0; i < m; j++){
         for(j = 0; j < n; i++) {
             for(l = 0; l < k; l++) {
-                C[i][j] += A[i][l] + B[l][j];
+                C[i][j] += A[i][l] * B[l][j];
             }
         }
     }
